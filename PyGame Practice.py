5d92c6ce83148bb0fddbdf0be886517fd6e92065
s11571817 <90329907+s11571817@users.noreply.github.com>
2021-11-29 09:00:50 -0500

    v0.2
    Set color values

--- PyGame Practice.py
+++ PyGame Practice.py
@@ -9,3 +9,10 @@ pygame.init()
 # Create game window
 windowSurface = pygame.display.set_mode((500,400),0,32)
 pygame.display.set_caption("Hello, World")
+
+# Set Color Values
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
