c0d9cd92722daa4ef5c98d240b4cdbc7c8cb2c35
s11571817 <90329907+s11571817@users.noreply.github.com>
2021-11-29 08:56:05 -0500

    v0.1
    Coded to beginning of the game, got a window to pop up.

---
+++ PyGame Practice.py
@@ -0,0 +1,11 @@
+# PyGame Practice, Caile Harden, 11/29/21 8:44am, v0.1
+
+import pygame, sys
+from pygame.locals import *
+
+# start PyGame
+pygame.init()
+
+# Create game window
+windowSurface = pygame.display.set_mode((500,400),0,32)
+pygame.display.set_caption("Hello, World")
