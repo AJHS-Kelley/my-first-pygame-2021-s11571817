0c5ac7d244431a1d61b862f095dc660fc30ea96c
s11571817 <90329907+s11571817@users.noreply.github.com>
2021-12-01 08:52:23 -0500

    v0.8
    Made some changes to the code just to experiment with it. Code is functional.

--- PyGame Practice.py
+++ PyGame Practice.py
@@ -1,4 +1,4 @@
-# PyGame Practice, Caile Harden, 12/1/21 8:45am, v0.7
+# PyGame Practice, Caile Harden, 12/1/21 8:52am, v0.8
 
 import pygame, sys
 from pygame.locals import *
@@ -8,7 +8,7 @@ pygame.init()
 
 # Create game window
 windowSurface = pygame.display.set_mode((500,400),0,32)
-pygame.display.set_caption("Hello, World")
+pygame.display.set_caption("Hey!!!")
 
 # Set Color Values
 BLACK = (0, 0, 0)
@@ -19,12 +19,13 @@ BLUE = (0, 0, 255)
 MAROON = (128, 0, 0)
 PERIWINKLE = (179, 179, 255)
 SAGE = (159, 223, 159)
+ORANGE = (255, 117, 26)
 
 # Setup Fonts
 basicFont = pygame.font.SysFont(None,48)
 
 # SetUp Text
-text = basicFont.render('Hello, World', True, WHITE, BLUE) 
+text = basicFont.render('Hey!!!', True, WHITE, BLUE) 
 textRect = text.get_rect()
 textRect.centerx = windowSurface.get_rect().centerx
 textRect.centery = windowSurface.get_rect().centery
@@ -37,14 +38,14 @@ pygame.draw.polygon(windowSurface, GREEN, ((146, 0), (291, 106), (236, 277), (56
 
 # Draw blue lines on the windows surface
 pygame.draw.line(windowSurface, BLUE, (60, 60), (120, 60), 4)
-pygame.draw.line(windowSurface, BLUE, (120, 60), (60, 120))
+pygame.draw.line(windowSurface, ORANGE, (120, 60), (60, 120))
 pygame.draw.line(windowSurface, BLUE, (60, 120), (120, 120), 4)
-
-# Draw a rec circle
-pygame.draw.circle(windowSurface, RED, (300, 50), 20, 0)
 
 #Draw an ellipse
 pygame.draw.ellipse(windowSurface, MAROON, (300, 250, 40, 80), 1)
+
+# Draw a red circle
+pygame.draw.circle(windowSurface, RED, (300, 50), 20, 0)
 
 # Draw a text background rectangle onto surface
 pygame.draw.rect(windowSurface, BLUE, (textRect.left - 20, textRect.top - 20, textRect.width + 40, textRect.height + 40))
@@ -66,10 +67,4 @@ while True:
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
-
-
-
-
-
-
 
